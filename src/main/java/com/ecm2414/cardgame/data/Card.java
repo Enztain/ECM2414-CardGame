553d0d3c9125ffb3b80300739905ddd669f983da
Enztain <Enztain>
2025-10-05 18:36:11 +0100

    Javadoc for Card.java

--- src/main/java/com/ecm2414/cardgame/data/Card.java
+++ src/main/java/com/ecm2414/cardgame/data/Card.java
@@ -3,20 +3,37 @@ package com.ecm2414.cardgame.data;
 public final class Card {
     private final int value;
 
+    /**
+     *
+     * @param value passes the value of a card
+     */
     public Card(int value) {
         if (value < 0) throw new IllegalArgumentException("Card value must be non-negative.");
         this.value = value;
     }
 
+    /**
+     *
+     * @return returns int value of a card
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     *
+     * @return string representation of a class
+     */
     @Override
     public String toString() {
         return "Card(" + value + ")";
     }
 
+    /**
+     *
+     * @param obj passes obj, pressumably card
+     * @return whether the cards' values are equal
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -25,6 +42,9 @@ public final class Card {
         return this.value == other.value;
     }
 
+    /**
+     * @return an integer hash code representing this card
+     */
     @Override
     public int hashCode() {
         return Integer.hashCode(value);
